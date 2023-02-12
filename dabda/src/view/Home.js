6f5ebcfa5145a5cf3dc29891e045794fcd8970bf
wonsang <qkqk2938@naver.com>
2023-02-12 21:14:10 +0900

    3

--- dabda/src/view/Home.js
+++ dabda/src/view/Home.js
@@ -1,10 +1,16 @@
 import logo from '../image/logo/logo_mun.png'
+import { useNavigate } from "react-router-dom";
 
 
 const Home = () => {
+    const movePage = useNavigate();
+
+    function gohome(){
+      movePage('/dabda/main');
+    }
     return (
       <div id = "mainlogodiv">
-        <img id = "mainlogo" src ={logo} alt="logo" />
+        <img id = "mainlogo" src ={logo} alt="logo" onClick={gohome}/>
       </div>
     );
   };
