8f9139e68d686a2cf496a406fa63bd9214ab6b98
wonsang <qkqk2938@naver.com>
2023-02-21 06:41:13 +0000

    0221

--- dabda/src/view/dabda/Notice.js
+++ dabda/src/view/dabda/Notice.js
@@ -2,44 +2,47 @@ import React from 'react';
 import '../../css/dabda/notice.css';
 import {BrowserView, MobileView} from "react-device-detect";
 import axios from 'axios';
-import $ from 'jquery';
 //import { useNavigate } from "react-router-dom";
+class Notice extends React.Component {
 
-const Notice = () => {
+  state = {
+    gnotice: []
 
-   // const movePage = useNavigate();
-  axios.post('/api/notice/getNoticeList')
-  .then((Response)=>{
-    var data = Response.data.data;
-    console.log(data);
-    for(var i = 0 ; i < data.length; i++){
-      var row = data[i];
-      $("#notiTable").append(
-        `
-          <tr id = "row`+row.id+`.">
-            <th>`+row.id+`</th>
-            <th>`+row.title+`</th>
-            <th>`+row.requester+`</th>
-            <th>`+row.reqdate+`</th>
-            <th>`+row.hit+`</th>
-          </tr>
-        `
-      );
- 
+  }
 
-    }
-  })
-  .catch((Error)=>{console.log(Error)});
+  getNoticeList = async () => {
 
+    const result = await axios.post('/api/notice/getNoticeList');
+    const gnotice = result.data.data;
+    this.setState({ gnotice });
+  }
 
+  // const movePage = useNavigate();
+  componentDidMount() {
+    this.getNoticeList();
+  }
+
+  render() {
+    const notice = this.state.gnotice;
     return (
       <>
       <MobileView>
-        <div id = "noticeItems">
-          <div id = "noticeItem">
+        <div id = "MBnoticeItems">
+          <div id = "MBnoticeItem">
             <h2>NOTICE</h2>
               <table id = 'notiTable'>
-
+                  <tr>
+                    <th id = "culId">No.</th>
+                    <th id = "culTitle">제목</th>
+                    <th id = "culReq">작성자</th>
+                  </tr>
+                {notice.map((row, index) => (
+                    <tr id = "row{row.id}">
+                      <td className='tdId'>{row.id}. </td>
+                      <td className='tdTitle'>{row.title}</td>
+                      <td className='tdReq'>{row.requester}</td>
+                    </tr>        
+                ))}
             </table>
           </div>
         </div>
@@ -49,13 +52,86 @@ const Notice = () => {
           <div id = "noticeItem">
             <h2>NOTICE</h2>
               <table id = 'notiTable'>
-            
+                  <tr>
+                    <th id = "culId">No.</th>
+                    <th id = "culTitle">제목</th>
+                    <th id = "culReq">작성자</th>
+                    <th id = "culReqd">작성일</th>
+                    <th id = "culHit">조회</th>
+                  </tr>
+                {notice.map((row, index) => (
+                    <tr id = "row{row.id}">
+                      <td className='tdId'>{row.id}. </td>
+                      <td className='tdTitle'>{row.title}</td>
+                      <td className='tdReq'>{row.requester}</td>
+                      <td className='tdReqd'>{row.reqdate}</td>
+                      <td className='tdHit'>{row.hit}</td>
+                    </tr>        
+                ))}
             </table>
           </div>
         </div>
       </BrowserView>
       </>
     );
-  };
+
+  }
+
+
+}
+
+
+// const Notice = () => {
+//   var data = [];
+//    // const movePage = useNavigate();
+//   axios.post('/api/notice/getNoticeList')
+//   .then((Response)=>{
+//     console.log(Response.data.data)
+//     data = Response.data.data;
+//   })
+//   .catch((Error)=>{console.log(Error)});
+
+//   console.log(data);
+  
+
+//     return (
+//       <>
+//       <MobileView>
+//         <div id = "MBnoticeItems">
+//           <div id = "MBnoticeItem">
+//             <h2>NOTICE</h2>
+//               <table id = 'notiTable'>
+//                 {data.map((row, index) => (
+//                     <tr>
+//                       <th>{row.id}</th>
+//                       <th>{row.title}</th>
+//                       <th>{row.requester}</th>
+//                     </tr>        
+//                 ))}
+//             </table>
+//           </div>
+//         </div>
+//       </MobileView>
+//       <BrowserView>
+//         <div id = "noticeItems">
+//           <div id = "noticeItem">
+//             <h2>NOTICE</h2>
+//               <table id = 'notiTable'>
+//                 {data.map((row, index) => (
+//                     <tr id = "row{row.id}">
+//                       <th>{row.id}</th>
+//                       <th>{row.title}</th>
+//                       <th>{row.requester}</th>
+//                       <th>{row.reqdate}</th>
+//                       <th>{row.hit}</th>
+//                     </tr>        
+//                 ))}
+//             </table>
+//           </div>
+//         </div>
+//       </BrowserView>
+//       </>
+//     );
+//   };
   
   export default Notice;
